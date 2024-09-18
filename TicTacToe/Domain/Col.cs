36e6093f0f1cf278deefbf58307eccb5c453cdf1
saeed khorsand <saeed.mkhorsand@gmail.com>
2024-09-18 19:00:53 -0400

    Add Col and Player classes to domain
    Introduced Col and Player classes to the TicTacToe.Domain namespace. The Col class tracks the position and player selection, while the Player class stores

---
+++ TicTacToe/Domain/Col.cs
@@ -0,0 +1,14 @@
+﻿namespace TicTacToe.Domain;
+
+public class Col
+{
+    public Col(short xPosition, short yPosition)
+    {
+        XPosition = xPosition;
+        YPosition = yPosition;
+    }
+    public Player? SelectedBy { get; set; }
+    public short XPosition { get; set; }
+    public short YPosition { get; set; }
+    public short Order => Convert.ToInt16($"{XPosition}{YPosition}");
+}
