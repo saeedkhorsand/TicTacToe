26daef69edd7d355c714733e65e5f774af0a27e5
saeed khorsand <saeed.mkhorsand@gmail.com>
2024-09-18 19:02:43 -0400

    Add Game and GameRunner classes to TicTacToe application
    Introduced `Game` and `GameRunner` classes to facilitate the main game logic and interaction loop for a TicTacToe application. The `Game` class includes methods for board management and win detection, while `GameRunner` handles player turns and user input.

---
+++ TicTacToe/Application/Game.cs
@@ -0,0 +1,98 @@
+﻿using TicTacToe.Domain;
+
+namespace TicTacToe.Application;
+
+public enum Way
+{
+    Right,
+    Down,
+    RightDown,
+    LeftDown
+}
+
+public class Game
+{
+    private int Size { get; set; } = 3;
+    private string Name { get; set; } = "Sample";
+
+    private List<Col> Cols { get; set; } = new List<Col>();
+
+    public Game(string name, int size)
+    {
+        Size = size;
+        Name = name;
+
+        for (short x = 0; x < Size; x++)
+        {
+            for (short y = 0; y < Size; y++)
+            {
+                Cols?.Add(new Col(x, y));
+            }
+        }
+    }
+
+    public void WriteOnScreen()
+    {
+        Console.Clear();
+        Console.WriteLine("Current Board:");
+        for (short x = 0; x < Size; x++)
+        {
+            string row = "";
+            for (short y = 0; y < Size; y++)
+            {
+                var col = GetCol(x, y);
+                if (col.SelectedBy == null)
+                {
+                    row += $"{x}{y} ";
+                }
+                else
+                {
+                    row += $"{col.SelectedBy.Name} ";
+                }
+            }
+            Console.WriteLine(row);
+        }
+    }
+
+    public Col? GetCol(short xpos, short ypos, Player? player = null)
+    {
+        return Cols.FirstOrDefault(x =>
+            x.XPosition == xpos && x.YPosition == ypos && (player == null || x.SelectedBy == player));
+    }
+
+    public (bool result, string message) SetColPlayer(short xpos, short ypos, Player player)
+    {
+        var col = GetCol(xpos, ypos);
+        if (col == null)
+            return (false, "Col Not Exists");
+        if (col.SelectedBy != null)
+            return (false, $"It Chosen by {col.SelectedBy.Name!} Before");
+        col.SelectedBy = player;
+        return (true, "Done");
+    }
+
+    public (bool result , Player? player) HaveWinner()
+    {
+        var players = Cols.GroupBy(x => x.SelectedBy).Select(x => x.Key).Where(x => x != null).ToList();
+
+        // Build a tree for each player and check for a winner
+        foreach (var player in players)
+        {
+            var gameTree = new GameTree(Cols, Size);
+            var playerTree = gameTree.BuildTree(player!);
+
+            // Traverse the tree to check if the player has won
+            if (gameTree.CheckWinner(playerTree))
+            {
+                Console.WriteLine($"Player {player!.Name} wins!");
+                return (true,player);
+            }
+        }
+
+        Console.WriteLine("No winner yet.");
+        return (false,null);
+    }
+}
+
+
+
---
+++ TicTacToe/Application/GameRunner.cs
@@ -0,0 +1,61 @@
+﻿using TicTacToe.Domain;
+
+namespace TicTacToe.Application;
+
+public class GameRunner
+{
+    private Game game { get; set; }
+    private List<Player> Players = new();
+    private Player? nowIsTurn { get; set; }
+
+    public GameRunner(string gameName, int gameSize)
+    {
+        game = new Game(gameName, gameSize);
+        Players.Add(new Player("P1"));
+        Players.Add(new Player("P2"));
+    }
+
+    public void Start()
+    {
+        do
+        {
+            Act();
+            game.WriteOnScreen();
+        } while (!game.HaveWinner().result);
+    }
+
+    public Game Game => game;
+
+    private string Act()
+    {
+        nowIsTurn ??= Players.First();
+        Console.WriteLine($" GO NOW {nowIsTurn.Name} : ");
+        var xPosition = (short)GetFromUser<int>("Enter X: ")!;
+        var yPosition = (short)GetFromUser<int>("Enter Y: ")!;
+        var setColResult = game.SetColPlayer(xPosition, yPosition, player: nowIsTurn);
+
+        if (setColResult.result)
+        {
+            nowIsTurn = nowIsTurn == Players.First() ? Players.Last() : Players.First();
+            return setColResult.message;
+        }
+    
+        Console.WriteLine(setColResult.message);
+        Console.WriteLine("Try Again");
+        return Act();
+    }
+
+    private T? GetFromUser<T>(string message)
+    {
+        Console.WriteLine(message);
+        try
+        {
+            return (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"Field to get data, try again {exception.Message}");
+            return GetFromUser<T>(message);
+        }
+    }
+}
