4de012346b97135ed6ece4d62e3c80ec8fc2f040
saeed khorsand <saeed.mkhorsand@gmail.com>
2024-09-18 19:02:59 -0400

    Add initial TicTacToe project setup
    Created the main program file with a basic game loop and the project file for the .NET project. This includes configuration settings for target framework, assembly name, and namespace.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TicTacToe.Application;
+
+var simplegame = new GameRunner("",3);
+simplegame.Game.WriteOnScreen();
+simplegame.Start();
